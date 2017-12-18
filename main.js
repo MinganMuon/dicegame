4fe8d5f7223fcd17550c022381b1de896082f1d1
MinganMuon <mingan-muon@outlook.com>
2017-12-17 22:57:12 -0600

    Rewrote setup functions to incorporate board and row classes, all four colored rows, and easier (but not anywhere close to perfect) sprite initalization.

--- main.js
+++ main.js
@@ -1,129 +1,106 @@
 // main.js
 
 function Main() {
-    var self = this
-    // init code at bottom
+    var self = this;
 
-    // set up tiles
-    this.tileOnClick = function() {
-        locInTexArray = Math.abs(this.number - 13); // big hack here
-        if (this.checked == true){
-            this.checked = false;
-            this.setTexture(self.redtiletex[locInTexArray]);
+    this.setuprow = function(rownumber, uctex, ctex) {
+        let startno = self.theboard.rows[rownumber].startnum;
+        let endno = self.theboard.rows[rownumber].endnum;
+
+        // the following block can definitely be improved upon
+        // especially the i's, j's, and the stuff reliant upon those variables
+        if (startno > endno) {
+            // decr
+            for (let i = 12; i > 0; i--){
+                let j = i; if (i == 1) { j = 0;}
+                let rectangle = new PIXI.Rectangle(50*(j%4), 50*(Math.floor(j/4)), 50, 50);
+                let tile_tex_checked = new PIXI.Texture(ctex, rectangle);
+                let tile_tex = new PIXI.Texture(uctex, rectangle);
+                
+                let tile = new PIXI.Sprite(tile_tex);
+                tile.y = 10 + (rownumber)*tile.height + (rownumber)*5;
+                tile.x = 10 + (12-i)*tile.width + 5*(12-i);
+                tile.interactive = true;
+                // tile.on('pointerdown', this.tileOnClick);
+                tile.checkedtex = tile_tex_checked;
+                tile.uncheckedtex = tile_tex;
+                if (i == 1) {
+                    tile.number = -1;
+                } else {
+                    tile.number = (12-i) - 1; // -1 because arrays are zero-indexed
+                }
+                this.app.stage.addChild(tile);
+            }
         } else {
-            this.checked = true;
-            this.setTexture(self.redtiletexchecked[locInTexArray]);
-        }
-    }
-    this.doredtiles = function(i) {
-        let j = i; if (j == 13) { j = 0; }
-        let rectangle = new PIXI.Rectangle(50*(j%4), 50*(Math.floor(j/4)), 50, 50);
-    
-        let tile_tex_checked = new PIXI.Texture(this.red_tiles_tex_checked, rectangle);
-        this.redtiletexchecked.push(tile_tex_checked);
-        let tile_tex = new PIXI.Texture(this.red_tiles_tex, rectangle);
-        this.redtiletex.push(tile_tex);
-    
-        let tile = new PIXI.Sprite(tile_tex);
-        tile.y = 10;
-        tile.x = 10 + (i-2)*tile.width + 5*(i-2);
-        tile.interactive = true;
-        tile.on('pointerdown', this.tileOnClick);
-        tile.color = "red";
-        tile.number = i;
-        tile.checked = false;
-        this.redtiles.push(tile);
-        this.app.stage.addChild(tile);
-    }
-    this.setuptiles = function() {
-        this.redtiles = [];
-        this.redtiletex = [];
-        this.redtiletexchecked = [];
-        this.red_tiles_tex = PIXI.utils.TextureCache["img/red_tiles.png"];
-        this.red_tiles_tex_checked = PIXI.utils.TextureCache["img/red_tiles_checked.png"];
-        for (let i = 13; i > 1; i--){
-            this.doredtiles(i);
+            // incr
+            for (let i = 2; i < 14; i++){
+                let j = i; if (j == 13) { j = 0; }
+                let rectangle = new PIXI.Rectangle(50*(j%4), 50*(Math.floor(j/4)), 50, 50);
+                let tile_tex_checked = new PIXI.Texture(ctex, rectangle);
+                let tile_tex = new PIXI.Texture(uctex, rectangle);
+                
+                let tile = new PIXI.Sprite(tile_tex);
+                tile.y = 10 + (rownumber)*tile.height + (rownumber)*5;
+                tile.x = 10 + (i-2)*tile.width + 5*(i-2);
+                tile.interactive = true;
+                // tile.on('pointerdown', this.tileOnClick);
+                tile.checkedtex = tile_tex_checked;
+                tile.uncheckedtex = tile_tex;
+                if (i == 13) {
+                    tile.number = -1;
+                } else {
+                    tile.number = (i - 2) - 1; // -1 because arrays are zero-indexed
+                }
+                this.app.stage.addChild(tile);
+            }
         }
     }
 
-    // init
-    console.log("Entered Main()");
+    this.setupboard = function() {
+        console.log("Textures loaded.");
+        console.log("Setting up the board...");
+        this.theboard = new board();
+        console.log("Initalizing sprites...");
+        // red row
+        let red_tiles_tex = PIXI.utils.TextureCache["img/red_tiles.png"];
+        let red_tiles_tex_checked = PIXI.utils.TextureCache["img/red_tiles_checked.png"];
+        this.setuprow(0, red_tiles_tex, red_tiles_tex_checked);
+        console.log("Red sprites done");
+        // yellow row
+        let yellow_tiles_tex = PIXI.utils.TextureCache["img/yellow_tiles.png"];
+        let yellow_tiles_tex_checked = PIXI.utils.TextureCache["img/yellow_tiles_checked.png"];
+        this.setuprow(1, yellow_tiles_tex, yellow_tiles_tex_checked);
+        console.log("Yellow sprites done");
+        // green row
+        let green_tiles_tex = PIXI.utils.TextureCache["img/green_tiles.png"];
+        let green_tiles_tex_checked = PIXI.utils.TextureCache["img/green_tiles_checked.png"];
+        this.setuprow(2, green_tiles_tex, green_tiles_tex_checked);
+        console.log("Green sprites done");
+        // blue row
+        let blue_tiles_tex = PIXI.utils.TextureCache["img/blue_tiles.png"];
+        let blue_tiles_tex_checked = PIXI.utils.TextureCache["img/blue_tiles_checked.png"];
+        this.setuprow(3, blue_tiles_tex, blue_tiles_tex_checked);
+        console.log("Blue sprites done");
+        console.log("All sprites done.");
+    }
+
+    console.log("Initializing...");
     this.app = new PIXI.Application({transparent: true});
-
     this.app.renderer.view.style.position = "absolute";
     this.app.renderer.view.style.display = "block";
     this.app.renderer.autoResize = true;
     this.app.renderer.resize(window.innerWidth, window.innerHeight);
-    
     document.body.appendChild(this.app.view);
+
+    console.log("Loading textures...");
     PIXI.loader
     .add("img/red_tiles.png")
     .add("img/red_tiles_checked.png")
-    .load(this.setuptiles.bind(this));
+    .add("img/yellow_tiles.png")
+    .add("img/yellow_tiles_checked.png")
+    .add("img/green_tiles.png")
+    .add("img/green_tiles_checked.png")
+    .add("img/blue_tiles.png")
+    .add("img/blue_tiles_checked.png")
+    .load(this.setupboard.bind(this));
 }
-
-/*
-Main.prototype.doredtiles = function(i) {
-    let j = i; if (j == 13) { j = 0; }
-    let rectangle = new PIXI.Rectangle(50*(j%4), 50*(Math.floor(j/4)), 50, 50);
-
-    let tile_tex_checked = new PIXI.Texture(Main.red_tiles_tex_checked, rectangle);
-    Main.redtiletexchecked.push(tile_tex_checked);
-    let tile_tex = new PIXI.Texture(Main.red_tiles_tex, rectangle);
-    Main.redtiletex.push(tile_tex);
-
-    let tile = new PIXI.Sprite(tile_tex);
-    tile.y = 10;
-    tile.x = 10 + (i-2)*tile.width + 5*(i-2);
-    tile.interactive = true;
-    tile.on('pointerdown', Main.tileOnClick);
-    tile.color = "red";
-    tile.number = i;
-    tile.checked = false;
-    Main.redtiles.push(tile);
-    Main.app.stage.addChild(tile);
-}
-
-Main.prototype.tileOnClick = function() {
-    locInTexArray = Math.abs(this.number - 13); // big hack here
-    if (this.checked == true){
-        this.checked = false;
-        this.setTexture(Main.redtiletex[locInTexArray]);
-    } else {
-        this.checked = true;
-        this.setTexture(Main.redtiletexchecked[locInTexArray]);
-    }
-}
-
-Main.prototype.setuptiles = function() {
-    // init redtiles
-    Main.redtiles = [];
-    Main.redtiletex = [];
-    Main.redtiletexchecked = [];
-    Main.red_tiles_tex = PIXI.utils.TextureCache["img/red_tiles.png"];
-    Main.red_tiles_tex_checked = PIXI.utils.TextureCache["img/red_tiles_checked.png"];
-    for (let i = 13; i > 1; i--){
-        Main.doredtiles(i);
-    }
-}
-
-Main.prototype.init = function() {
-    Main.app = new PIXI.Application({transparent: true});
-
-    Main.app.renderer.view.style.position = "absolute";
-    Main.app.renderer.view.style.display = "block";
-    Main.app.renderer.autoResize = true;
-    Main.app.renderer.resize(window.innerWidth, window.innerHeight);
-    
-    document.body.appendChild(Main.app.view);
-    console.log(Main);
-    PIXI.loader
-    .add("img/red_tiles.png")
-    .add("img/red_tiles_checked.png")
-    .load(Main.setuptiles());
-}
-
-function Main() {
-    this.init();
-}
-*/
